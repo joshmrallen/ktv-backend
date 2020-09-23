e4b0c51532cf7fbd1b892e37cb769b3ab7b2d58e
github username <joshmrallen@gmail.com>
2020-09-23 17:14:10 -0400

    resources created and .gitignore added to

---
+++ db/migrate/20200923210502_create_videos.rb
@@ -0,0 +1,11 @@
+class CreateVideos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :videos do |t|
+      t.string :name
+      t.string :lyrics
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
