fd5a8ae6510e23ea3471d07267772c1ff1538c54
github username <joshmrallen@gmail.com>
2020-09-24 12:02:28 -0400

    started post request from front-end for search query -- see users controller/routes/user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def search_handler
+        
+
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+
+    def self.search(query)
+        key = ENV['API_KEY']
+        response = RestClient.get("https://www.googleapis.com/youtube/v3/search?maxResults=10&q=#{query}h&key=#{key}")
+        result = JSON.parse(response.body)
+    end
+
 end
