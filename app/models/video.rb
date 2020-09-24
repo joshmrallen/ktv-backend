8462f35e97d63e2f8b6dfe936d85b577c31cd01e
redazures <jwu_nyc@yahoo.com>
2020-09-24 10:30:37 -0400

    method

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ApplicationRecord
 
     def self.search (query)
         key = ENV['API_KEY']
-        response = RestClient.get("https://www.googleapis.com/youtube/v3/search?q=#{query}h&key=#{key}")
+        response = RestClient.get("https://www.googleapis.com/youtube/v3/search?maxResults=10&q=#{query}h&key=#{key}")
         result = JSON.parse(response.body)
     end
       
