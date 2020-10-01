64a0a9fe02a2d83fea3a6408b9a2de2fd47dd199
github username <joshmrallen@gmail.com>
2020-10-01 16:31:04 -0400

    tried to get lyrics and failed

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,3 +1,26 @@
 class VideosController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def index
+        videos = Video.all
+        render json: videos
+    end
+
+    def show
+        video = Video.find(video_params)
+        render json: video
+    end
+
+    def create
+        video = Video.create_or_find_by(youTubeId: video_params)
+        video.get_video_details
+        render json: video
+    end
+
+    private
+
+    def video_params
+        params.require(:video).permit(:youTubeId)
+    end
     
 end
